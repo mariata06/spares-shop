b79fe7ad2a34f5ad0a641746b83eaec0aebdd5b4
mariata06 <slinkywizard@mail.ru>
2021-11-08 20:41:06 +0300

    Верстка декоративных деталей фильтра, меню-аккордеон, переключение вкладок параметры - по марке, сброс настроек фильтра

---
+++ build/js/filter.js
@@ -0,0 +1,69 @@
+// Переключение вкладок в фильтре поиска шин - Параметры - по Марке
+var chooseFilterItem = function () {
+  var filterChoiceButton = document.querySelectorAll(".tire-filter__choice-button");
+  var filterChoiceItem = document.querySelectorAll(".tire-filter__choice-item");
+  var filterChoiceName;
+
+  filterChoiceButton.forEach(function (item) {
+    item.addEventListener('click', toggleFilterChoiceItem);
+  });
+
+  function toggleFilterChoiceItem() {
+    filterChoiceButton.forEach(function (item) {
+      item.classList.remove("tire-filter__choice-button--active");
+    });
+    this.classList.add("tire-filter__choice-button--active");
+    console.log(this);
+    filterChoiceName = this.getAttribute("data-tab-name");
+    selectFilterContent(filterChoiceName);
+  }
+
+  function selectFilterContent(filterChoiceName) {
+    filterChoiceItem.forEach(function (item) {
+      if (item.classList.value.includes(filterChoiceName)) {
+        item.classList.add("tire-filter__choice-item--active");
+      } else {
+        item.classList.remove("tire-filter__choice-item--active");
+      }
+    });
+  }
+};
+
+chooseFilterItem();
+
+// Переключение меню - аккордеона в фильтре подбора шин
+var filterItemTriggers = document.querySelectorAll(".tire-filter__params-trigger");
+
+filterItemTriggers.forEach(function (item) {
+  item.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    item.classList.toggle("tire-filter__params-trigger--active");
+    var accItemcontent = item.nextElementSibling;
+    accItemcontent.classList.toggle("tire-filter__params-content--active");
+  });
+});
+
+// Сброс настроек фильтра после выбранных параметров
+var resetFilterBtn = document.querySelector(".tire-filter__button--reset");
+resetFilterBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  document.querySelectorAll("input").forEach(function (item) {
+    item.checked = false;
+  });
+  document.querySelectorAll('[type="search"]').forEach(function (item) {
+    item.value = "";
+  });
+});
+
+// Открытие фильтра на мобиле
+// var openFilterBtn = document.querySelector(".sort__filter-button");
+// var tireFilter = document.querySelector(".filter");
+// var closeFilterBtn = document.querySelector(".tire-filter__close");
+// openFilterBtn.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   tireFilter.classList.add("tire-filter--open");
+// });
+// closeFilterBtn.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   tireFilter.classList.remove("tire-filter--open");
+// });
